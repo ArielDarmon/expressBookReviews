491cdd18eb5ed2c337c98f1cc44a4730b1eac68b
Ariel Darmon <19586672+ArielDarmon@users.noreply.github.com>
2024-01-14 11:23:47 +0200

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": { 1:"This is a review about 1" }, "isbn":1 },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": { 1:"This is a review about 2" }, "isbn":2 },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": { 1:"This is a review about 3" }, "isbn":3 },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": { 1:"This is a review about 4" }, "isbn":4 },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": { 1:"This is a review about 5" }, "isbn":5 },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": { 1:"This is a review about 6" }, "isbn":6 },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": { 1:"This is a review about 7" }, "isbn":7 },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": { 1:"This is a review about 8" }, "isbn":8 },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": { 1:"This is a review about 9", 2:"This is another review about 9" }, "isbn":9 },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": { 1:"This is a review about 10" }, "isbn":10 }
 }
 
 module.exports=books;
