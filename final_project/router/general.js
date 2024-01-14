274615e2192d881d3b61d8b0daad4fb06500b808
Ariel Darmon <19586672+ArielDarmon@users.noreply.github.com>
2024-01-14 10:06:56 +0200

    Update general.js
    add reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,35 +18,54 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    const books_array = Object.values(books);
-    //console.log(books_array);
+    let books_array = Object.values(books);
     for(i=0; i<books_array.length; i++) {
         let books_item = books_array[i];
-        if(books_item.isbn === isbn) {
+        if((books_item.isbn) == isbn) {
             return res.send(books_item);
         }
     }
-    
-    
-    //return res.status(200);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let books_array = Object.values(books);
+    let books_items = new Array();
+    for(i=0; i<books_array.length; i++) {
+        let books_item = books_array[i];
+        if((books_item.author) == author) {
+            books_items.push(books_item);
+        }
+    }
+    return res.send(books_items);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let books_array = Object.values(books);
+    for(i=0; i<books_array.length; i++) {
+        let books_item = books_array[i];
+        if((books_item.title) == title) {
+            return res.send(books_item);
+        }
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let books_array = Object.values(books);
+    let book_reviews = new Array();
+    for(i=0; i<books_array.length; i++) {
+        let books_item = books_array[i];
+        if((books_item.isbn) == isbn) {
+            console.log(books_item.reviews);
+            book_reviews.push(Object.values(books_item.reviews));
+        }
+    }
+    return res.send(book_reviews);
 });
 
 module.exports.general = public_users;
